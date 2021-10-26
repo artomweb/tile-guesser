c90b169eb9e5799d57b5aaf7cd506355e1ec8ca8
Archie <artomweb@gmail.com>
2021-10-26 21:41:32 +0100

    webpage

--- sketch.js
+++ sketch.js
@@ -7,59 +7,53 @@ let guesses = 0;
 let found = false;
 let button;
 let attempts = 0;
-let avg = '~';
+let avg = "~";
 let totalScores = 0;
 
 function setup() {
-  createCanvas(400, 600);
-  boxWidth = (400 / gridSize);
-  boxHeight = (400 / gridSize);
-  
+  canvas = createCanvas(400, 600);
+  canvas.parent("sketch");
+  boxWidth = 400 / gridSize;
+  boxHeight = 400 / gridSize;
+
   keys = createVector(floor(random(gridSize)), floor(random(gridSize)));
-  
-    for (i = 0; i < gridSize; i++){
-    for (j = 0; j < gridSize; j++){
-      let boxTemp = new box(i,j)
+
+  for (i = 0; i < gridSize; i++) {
+    for (j = 0; j < gridSize; j++) {
+      let boxTemp = new box(i, j);
       boxes.push(boxTemp);
-          
-          
     }
   }
-  
-  rectMode(CENTER)
-  
-  button = createButton('RESET');
-  button.size(100,50);
-  button.style("border-radius", "10px");
-  button.style("font-size", "20px");
-  button.position(200 - button.width/2, 525);
-  button.mousePressed(reset);
-  
+
+  // rectMode(CENTER);
+
+  // let button = createButton("RESET");
+  // button.size(100, 50);
+  // button.style("border-radius", "10px");
+  // button.style("font-size", "20px");
+  // button.parent("sketch");
+  // button.position(0, 0);
+  // button.mousePressed(reset);
+
   // noLoop();
 }
 
-function reset(){
-  if (found){
+function reset() {
+  if (found) {
     attempts++;
-  totalScores += guesses;
-  avg = round(totalScores / attempts, 1);
+    totalScores += guesses;
+    avg = round(totalScores / attempts, 1);
   }
-  
+
   guesses = 0;
   found = false;
-  
-   keys = createVector(floor(random(gridSize)), floor(random(gridSize)));
-  
-  
-  for(let i = 0; i < boxes.length; i++){
-      boxes[i].generateNewProps();
+
+  keys = createVector(floor(random(gridSize)), floor(random(gridSize)));
+
+  for (let i = 0; i < boxes.length; i++) {
+    boxes[i].generateNewProps();
   }
-  
-  
-  
 }
-
-
 
 // function manhattan(vec1, vec2){
 //   return abs(vec1.x - vec2.x) + abs(vec1.y - vec2.y);
@@ -67,114 +61,118 @@ function reset(){
 
 function draw() {
   background(220);
-  
-  for(let i = 0; i < boxes.length; i++){
-      boxes[i].draw();
+
+  for (let i = 0; i < boxes.length; i++) {
+    boxes[i].draw();
   }
-  
+
   rectMode(CENTER);
-  
-  textAlign(CENTER, CENTER);
-  
+
+  textAlign(CENTER);
+
   fill(0);
-  
+
   textSize(32);
-  
+
   text("Guesses: " + guesses, 100, 475, 100, 100);
   text("Avg: " + avg, 300, 475, 50, 100);
-  
-
 }
 
-function mouseClicked(){
-  
-    if(!found){
-       for(let i = 0; i < boxes.length; i++){
-      if (boxes[i].mouseInside()){
-        if(!boxes[i].flipped){
+function mouseClicked() {
+  if (found) {
+    reset();
+  } else {
+    for (let i = 0; i < boxes.length; i++) {
+      if (boxes[i].mouseInside()) {
+        if (!boxes[i].flipped) {
           boxes[i].flipped = true;
           guesses++;
         }
-        
-        if(boxes[i].distKeys == 0){
+
+        if (boxes[i].distKeys == 0) {
           found = true;
         }
       }
-  }
     }
-  
-   
-  
+  }
 }
 
-class box{
-  constructor(i,j){
-    
-    this.gridPos = createVector(i,j);
-    this.realPos = createVector(this.gridPos.x * boxWidth + boxWidth / 2, this.gridPos.y * boxHeight + boxHeight / 2);
+class box {
+  constructor(i, j) {
+    this.gridPos = createVector(i, j);
+    this.realPos = createVector(
+      this.gridPos.x * boxWidth + boxWidth / 2,
+      this.gridPos.y * boxHeight + boxHeight / 2
+    );
     this.generateNewProps();
   }
-  
-  generateNewProps(){
+
+  generateNewProps() {
     this.distKeys = this.manhatten();
     this.hiddenCol = this.calcCol();
-    this.col = color('#FFFFF');
+    this.col = color("#FFFFF");
     this.width = boxWidth;
     this.height = boxHeight;
     this.flipped = false;
   }
-  
-  manhatten(){
-    return abs(keys.x - this.gridPos.x) + abs(keys.y - this.gridPos.y);  
+
+  manhatten() {
+    return abs(keys.x - this.gridPos.x) + abs(keys.y - this.gridPos.y);
   }
-  
-  calcCol(){
+
+  calcCol() {
     let boxCol;
-    switch(this.distKeys){
-          case(0):
-            boxCol = color('#F5F749');
-            break;
-            
-          case(1):
-            boxCol = color('#BD0B00');
-            break;
-            
-          case(2):
-            boxCol = color('#DF4747');
-            break;
-            
-          case(3):
-            boxCol = color('#D77FC9');
-            break;
-            
-          case(4):
-            boxCol = color('#B7BAFF');
-            break;
-            
-          default:
-            boxCol = color('#C1DCFD')
-            
-      }
+    switch (this.distKeys) {
+      case 0:
+        boxCol = color("#F5F749");
+        break;
+
+      case 1:
+        boxCol = color("#BD0B00");
+        break;
+
+      case 2:
+        boxCol = color("#DF4747");
+        break;
+
+      case 3:
+        boxCol = color("#D77FC9");
+        break;
+
+      case 4:
+        boxCol = color("#B7BAFF");
+        break;
+
+      default:
+        boxCol = color("#C1DCFD");
+    }
     return boxCol;
   }
-  
-  draw(){
-    
-    
+
+  draw() {
     rectMode(CENTER);
-    
-    if (this.flipped){
+
+    if (this.flipped) {
       fill(this.hiddenCol);
-      rect(this.realPos.x, this.realPos.y, this.width-10, this.height-10, 5);
-    }else{
+      rect(
+        this.realPos.x,
+        this.realPos.y,
+        this.width - 10,
+        this.height - 10,
+        5
+      );
+    } else {
       fill(this.col);
       rect(this.realPos.x, this.realPos.y, this.width, this.height, 5);
     }
-    
-    
   }
-  
-  mouseInside(){
-    return mouseX >= this.realPos.x - boxWidth/2 && mouseX < this.realPos.x + boxWidth/2 && mouseY >= this.realPos.y - boxHeight/2 && mouseY < this.realPos.y + boxHeight/2;
+
+  mouseInside() {
+    return (
+      mouseX >= this.realPos.x - boxWidth / 2 &&
+      mouseX < this.realPos.x + boxWidth / 2 &&
+      mouseY >= this.realPos.y - boxHeight / 2 &&
+      mouseY < this.realPos.y + boxHeight / 2
+    );
   }
 }
